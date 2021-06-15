bd3f957d4ad43b2213fbd7978b931024496b3602
Dmitry <dmitry.gr.web@gmail.com>
2021-06-15 17:43:41 +0300

    crmtablestart

---
+++ js/script.crm-table.js
@@ -0,0 +1,20 @@
+// const btnOrders = document.querySelector('.btn-link');
+// const btnWrap = document.querySelector('.crm-header-link');
+// btnWrap.addEventListener('click', (event) => {
+//     btnOrders.classList.toggle('btn-toggle');
+// });
+// btnOrders.forEach(btnOrders => {
+//     btnwrap.addEventListener('click', () => {
+//         btnOrders.classList.toggle('btn-toggle');
+//     })
+// });
+// btnWrap.forEach(btnWrap => {
+//     btnWrap.addEventListener('click', () => {
+//         btnOrders.classList.toggle('btn-toggle');
+//     })
+// });
+// $(".crm-header-link").click(function() {
+//     let btnArr = $('.crm-header-link .btn-link');
+//     btnArr.map(x => $(".crm-header-link .btn-link").removeClass('btn-toggle'));
+//     $('.btn-link').toggleClass('btn-toggle');
+// });
